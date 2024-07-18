57786cdb2c6c8448b5bcf5b2ab4ef0172c74ccf6
setiobudiafandi <setiobudi71@gmail.com>
2024-07-18 22:27:11 +0700

    adding test case with assertion of text inside

--- test/specs/mobile.js
+++ test/specs/mobile.js
@@ -1,5 +1,9 @@
 describe('Open apps', () => {
+
     it('Access Accessibility Node Provider', async() => {
         await $('//android.widget.TextView[@content-desc="Accessibility"]').click()
+        await $('//android.widget.TextView[@content-desc="Accessibility Node Provider"]').click()
+        const elementText = await $('//android.widget.TextView[@content-desc="Enable TalkBack and Explore-by-touch from accessibility settings. Then touch the colored squares."]').getText()
+        await expect(elementText).toContain("Enable")
     })
 })
