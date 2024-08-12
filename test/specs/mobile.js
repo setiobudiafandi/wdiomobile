5dc217adab172da53d37d768d5deffad6d4d7b80
setiobudiafandi <setiobudi71@gmail.com>
2024-08-12 11:30:46 +0700

    success running 2 sequential scenarios

--- test/specs/mobile.js
+++ test/specs/mobile.js
@@ -1,12 +1,17 @@
 
 
 describe('Open apps', () => {
+    // async function closeApp() {
+    //     await browser.execute('mobile: terminateApp', {bundleId: 'io.appium.android.apis'});
+    // }
 
     it('Access Accessibility Node Provider', async() => {
         await $('//android.widget.TextView[@content-desc="Accessibility"]').click()
         await $('//android.widget.TextView[@content-desc="Accessibility Node Provider"]').click()
         const elementText = await $('//android.widget.TextView[@content-desc="Enable TalkBack and Explore-by-touch from accessibility settings. Then touch the colored squares."]').getText()
         await expect(elementText).toContain("Enable")
+        await browser.execute('mobile: terminateApp', {appId: 'io.appium.android.apis'})
+        await browser.execute('mobile: activateApp', {appId: 'io.appium.android.apis'})
     })
 
     it('Input text on TextFields menu', async() => {
